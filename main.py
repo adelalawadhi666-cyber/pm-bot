679e944de50d129d56abc1d0ed7321cb572780b0
adelalawadhi666-cyber <adelalawadhi666@gmail.com>
2026-09-06 19:14:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
+import requests
 print("bot online")
 while True:
-    print("alive")
-    time.sleep(30)
+    try:
+        r = requests.get("https://gamma-api.polymarket.com/markets?closed=false&limit=1", timeout=20)
+        print("pm", r.status_code)
+    except Exception as e:
+        print("err")
+    time.sleep(60)
